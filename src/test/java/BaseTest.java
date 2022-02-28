aa898c870d91344307c0a1adccccf414c9062fb9
natalia-savinova <svn.1421@gmail.com>
2022-02-28 22:14:58 +0300

    add first test

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void firstTest() {
+        driver.navigate().to("http://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Form Authentication")).click();
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).click();
+
+        String result = driver.findElement(By.id("flash")).getText();
+
+        assertTrue(result.contains("You logged into a secure area!"), "Alert text is incorrect");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+}
